571ed5c02c3b16f59305ab706ee97c8e19c75f09
ciarasauer25 <143199234+ciarasauer25@users.noreply.github.com>
2025-08-01 08:11:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const CaixaPrincipal = document.querySelectorAll(".caixa-principal");
+const CaixaPergunta = document.querySelectorAll(".caixa-pergunta");
+const CaixaAlternativa = document.querySelectorAll(".caixa-alternativa");
+const CaixaResultado = document.querySelectorAll(".caixa-resultado");
+const TextoResultado = document.querySelectorAll(".texto-resultado");
+
+const Perguntas = [
+    {
+        enunciado:"pergunta 1",
+        alternativa:[
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    },
+    {
+        enunciado:"pergunta 2",
+        alternativa:[
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    },
+];
