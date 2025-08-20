af66abe0e48693ac2e20e4121b4df190f6e02663
ciarasauer25 <143199234+ciarasauer25@users.noreply.github.com>
2025-08-20 08:49:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,77 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado:"A paleontologia utiliza o estudo de fósseis para reconstruir ecossistemas antigos, compreender extinções em massa e traçar a evolução das espécies ao longo de milhões de anos. Entre os métodos utilizados estão a adaptação adiométrica, a análise de camadas geológicas e a comparação anatômica entre fósseis e seres vivos atuais",
-        alternativa:[  "alternativa 1","alternativa 2"  ]
+        enunciado: "A paleontologia utiliza o estudo de fósseis para reconstruir ecossistemas antigos, compreender extinções em massa e traçar a evolução das espécies ao longo de milhões de anos. Entre os métodos utilizados estão a adaptação adiométrica, a análise de camadas geológicas e a comparação anatômica entre fósseis e seres vivos atuais. Com base nisso, qual das alternativas descreve corretamente uma aplicação prática da paleontologia?",
+        alternativas: [
+            "A) Utilizar fósseis para prever catástrofes naturais em tempo real, como terremotos e furacões.",
+            "B) Analisar fósseis para entender como mudanças climáticas pasadas influenciaram a extinção de espécies."
+        ]
     },
     {
-        enunciado:"pergunta 2",
-        alternativa:[  "alternativa 1","alternativa 2"  ]
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+     {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
     },
     {
-        enunciado:"pergunta 3",
-        alternativa:[  "alternativa 1","alternativa 2"  ]
+        enunciado: "Pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
     },
     {
-        enunciado:"pergunta 4",
-        alternativa:[  "alternativa 1","alternativa 2"  ]
-    },
-    {
-        enunciado:"pergunta 5",
-        alternativa:[  "alternativa 1","alternativa 2"  ]
-    },
+        enunciado: "Pergunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
 ];
-    
-     let atual=0;
-     let perguntaAtual;
-     function mostraPergunta(){
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal;
+
+function mostraPergunta() {
+    if (atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
-     }
-     mostraPergunta()
+    caixaAlternativas.textContent="";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada (alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+} 
+
+mostraPergunta();
+
+function respostaSelecionada(opcaoSelecionada){
+   const afirmacoes= opcaoSelecionada.afirmacoes;
+   historiaFinal= afirmacoes;
+   historiaFinal  += afirmacoes +"";
+   atual++;
+   mostraPergunta();
+}
